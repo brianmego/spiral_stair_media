8a9132405d9c822406a2efd4485dff9dc9cabb6a
Brian Mego <brian.mego@q2ebanking.com>
2017-11-20 21:10:42 -0600

    Cleans up boostrap script with comments

--- robs_site_bootstrap_ubuntu.sh
+++ robs_site_bootstrap_ubuntu.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
+# Copy this script (and *.zip files in same directory) to an ubuntu machine and run it as root
+
+# Variables
+db_name=ssm
+username=root
+echo -n 'MySql Root Password: '
+read -s mysql_password
+
+# Update webserver
 apt update -y
 apt install unzip lamp-server^ -y
+
 cat << EOF > test.php
 <?php phpinfo() ?>
 EOF
+
 mv test.php /var/www/html/test.php
-mysqladmin -uroot create blog -p
+
+# MySQL setup
+mysqladmin -uroot create $db_name -p
 mysql_secure_installation
-echo -n 'MySql Root Password: '
-read -s mysql_password
 cd /var/www/html
+
+# Configure Wordpress
 wget http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 cd wordpress/
 mv wp-config-sample.php wp-config.php
-sed -i 's/database_name_here/blog/' wp-config.php
-sed -i 's/username_here/root/' wp-config.php
+sed -i "s/database_name_here/$db_name/" wp-config.php
+sed -i "s/username_here/$username/" wp-config.php
 sed -i "s/password_here/$mysql_password/" wp-config.php
 sed -i 's/upload_max_filesize.*/upload_max_filesize = 10M/' /etc/php/7.0/apache2/php.ini
 service apache2 reload
